a793d134f74453e4a16318babc9bee0a1077523c
Lucency <419010677@qq.com>
2017-07-29 12:39:30 +0800

    Update CrawlingNews.py

--- CrawlingNews.py
+++ CrawlingNews.py
@@ -1,14 +1,10 @@
 # -*- coding:utf-8 -*-
 import  urllib2
 
-#pagenum = pn 20 40 60 80 100 120
+totalpage = 15
 url = 'http://news.baidu.com/'
-# 关键词 
-keyword ='六度人和'
-
-for num in range(1,18):
-    half_url = u'ns?tn=news&from=news&cl=2&rn=20&ct=1&word=六度人和&pn=%s'%((num-1)*20)
-
+for num in range(1,totalpage):
+    half_url = u'ns?tn=news&from=news&cl=2&rn=20&ct=1&word=千米网&pn=%s'%((num-1)*20)
     #构造get请求
     req = urllib2.Request(url+half_url.encode('utf-8'))
     #添加header
@@ -16,11 +12,7 @@ for num in range(1,18):
     req.add_header('Referer',url)
     response = urllib2.urlopen(req)
     newsresponse = response.read()
-
-    totxt = open(keyword,'ab+')
+    filename = "qmw%s.html" %num
+    totxt = open(filename,'w')
     totxt.write(newsresponse)
     totxt.close()
-
-#数据分析
-#热新闻过滤：关键字，时间，媒体，标题，相同新闻数
-#u"年"==4
